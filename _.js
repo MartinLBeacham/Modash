f71ef9f65ec984420a24824310a40c51e8528348
MartinLBeacham <martinlutherbeacham@gmail.com>
2024-03-28 21:27:19 -0400

    Added pad function

--- _.js
+++ _.js
@@ -114,6 +114,52 @@ const _ = {
       return string.split(pattern);
     }
   },
+
+  pad(string, length = 0, chars = " ") {
+    if (string === undefined) {
+      return "";
+    }
+
+    if (typeof chars === "number" && typeof length === "string") {
+      let temp = chars;
+      chars = length;
+      length = temp;
+    }
+
+    if (length < 0) {
+      console.log("Only enter positive numbers for length.");
+      return undefined;
+    }
+
+    return this.basePad(string, length, chars);
+  },
+
+  basePad(string, length, chars) {
+    let originalLength = string.length;
+
+    if (originalLength >= length) {
+      return string;
+    }
+
+    const totalPaddingLength = length - originalLength;
+    const rightLength = Math.ceil(
+      (totalPaddingLength % (2 * chars.length)) / 2
+    );
+    const leftLength = Math.floor(
+      (totalPaddingLength % (2 * chars.length)) / 2
+    );
+
+    const sidePaddingMultiple = Math.floor(
+      totalPaddingLength / (2 * chars.length)
+    );
+
+    let rightPadding =
+      chars.repeat(sidePaddingMultiple) + chars.slice(0, rightLength);
+    let leftPadding =
+      chars.repeat(sidePaddingMultiple) + chars.slice(0, leftLength);
+
+    return leftPadding + string + rightPadding;
+  },
 };
 
 // Exporting _ object.
