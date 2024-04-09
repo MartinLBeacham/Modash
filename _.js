b3f110306caf7bebb759d736b985eb7e105880ac
MartinLBeacham <martinlutherbeacham@gmail.com>
2024-04-09 10:51:43 -0400

    Adding fetchAPI method.

--- _.js
+++ _.js
@@ -161,14 +161,14 @@ const _ = {
 
     const totalPaddingLength = length - originalLength;
     const rightLength = Math.ceil(
-      (totalPaddingLength % (2 * chars.length)) / 2
+      (totalPaddingLength % (2 * chars.length)) / 2,
     );
     const leftLength = Math.floor(
-      (totalPaddingLength % (2 * chars.length)) / 2
+      (totalPaddingLength % (2 * chars.length)) / 2,
     );
 
     const sidePaddingMultiple = Math.floor(
-      totalPaddingLength / (2 * chars.length)
+      totalPaddingLength / (2 * chars.length),
     );
 
     let rightPadding =
@@ -178,16 +178,16 @@ const _ = {
 
     return leftPadding + string + rightPadding;
   },
- /*
+  /*
   Input handler function for has.  Checks to see if arguments are of the correct type.
   Calls baseHas for functional code.
   */
   has(object, path) {
-    if (typeof path === 'string') {
-      path = path.split('.');
-    }else if(!Array.isArray(path)){
-      console.log("Path must be an array or string.")
-      return undefined
+    if (typeof path === "string") {
+      path = path.split(".");
+    } else if (!Array.isArray(path)) {
+      console.log("Path must be an array or string.");
+      return undefined;
     }
 
     return this.baseHas(object, path);
@@ -197,28 +197,25 @@ const _ = {
   Looks through a nested object to determine whether the provided path exists in that object.
   */
   baseHas(object, path) {
-
     if (Object.hasOwn(object, path[0])) {
-      
       if (path.length === 1) {
         return true;
       } else {
         object = object[path[0]];
-        return this.baseHas(object, path.slice(1,path.length));
+        return this.baseHas(object, path.slice(1, path.length));
       }
-    }
-    else {
+    } else {
       return false;
     }
   },
- /*
+  /*
   Input handler function for invert.  Checks to see if arguments are of the correct type.
   Calls baseInvert for functional code.
   */
-  invert(object){
-    if(typeof object !== 'object'){
-      console.log("The argument for object must be an Object!")
-      return object
+  invert(object) {
+    if (typeof object !== "object") {
+      console.log("The argument for object must be an Object!");
+      return object;
     }
     return this.baseInvert(object);
   },
@@ -227,47 +224,83 @@ const _ = {
   Inverts the key:value pairs so that the value is the key and the key is the value.
   key:value --> value:key
   */
-  baseInvert(object){
-
-    returnObject={};
-    for(key in object){
-      returnObject[object[key]]=key;
+  baseInvert(object) {
+    returnObject = {};
+    for (key in object) {
+      returnObject[object[key]] = key;
     }
     return returnObject;
   },
- /*
+  /*
   Input handler function for findKey. 
   Calls baseFindKey for functional code.
   */
-  findKey(object, predicate){
-
-    return this.baseFindKey(object, predicate)
-
+  findKey(object, predicate) {
+    return this.baseFindKey(object, predicate);
   },
   /*
   Functional code for findKey function.
   Takes an object as object and some search logic as predicate
   Currently predicate can either be a callback function or an array
   */
-  baseFindKey(object, predicate){
-
-    if(typeof predicate === 'function'){
-      for(key in object){
-        if(predicate(object[key])){
+  baseFindKey(object, predicate) {
+    if (typeof predicate === "function") {
+      for (key in object) {
+        if (predicate(object[key])) {
           return key;
-        }     
+        }
       }
-    }else{
-      for(key in object){
-        if(object[key][predicate[0]]===predicate[1]){
+    } else {
+      for (key in object) {
+        if (object[key][predicate[0]] === predicate[1]) {
           return key;
-        }     
+        }
       }
       return undefined;
     }
-
   },
 
+  fetchAPI() {
+    if (typeof baseUrl !== string) {
+      console.log("Please enter a string for baseUrl.");
+      return undefined;
+    }
+    if (typeof endpoint !== string) {
+      console.log("Please enter a string for endpoint.");
+      return undefined;
+    }
+    if (typeof requestParams !== string) {
+      console.log("Please enter a string for requestParams.");
+      return undefined;
+    }
+
+    if (typeof method !== string) {
+      console.log("Please enter a string for method.");
+      return undefined;
+    }
+    if (typeof headers !== object) {
+      console.log("Please enter a object for headers.");
+      return undefined;
+    }
+    return base_fetchAPI(baseUrl, endpoint, requestParams, method, headers);
+  },
+
+  async base_fetchAPI(baseUrl, endpoint, requestParams, method, headers) {
+    const urlToFetch = baseUrl + endpoint + requestParams;
+    console.log(urlToFetch);
+    try {
+      const response = await fetch(urlToFetch, {
+        method: "GET", // "POST" does not work
+        cache: "no-cache",
+        headers: headers ?? { "Content-Type": "application/json" },
+      });
+      if (response.ok) {
+        return response;
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  },
 };
 
 // Exporting _ object.
